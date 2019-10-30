cf0feffa7d300365004c254d9181408eb760e41f
raniaw <raniaw@web.de>
2019-10-30 12:24:04 +0100

    Updates

---
+++ precache-manifest.a388bed28a5459205a78932183bd36c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0e04f6e3536d304dff58c9687306ca3",
+    "url": "/hello-react-deploy/index.html"
+  },
+  {
+    "revision": "37c923e0d204dcb6de97",
+    "url": "/hello-react-deploy/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b30ff27ec87816c15dda",
+    "url": "/hello-react-deploy/static/js/2.bbc3b838.chunk.js"
+  },
+  {
+    "revision": "37c923e0d204dcb6de97",
+    "url": "/hello-react-deploy/static/js/main.005c114d.chunk.js"
+  },
+  {
+    "revision": "84f4a1da7f45c13f6861",
+    "url": "/hello-react-deploy/static/js/runtime-main.25426489.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/hello-react-deploy/static/media/logo.25bf045c.svg"
+  }
+]);
